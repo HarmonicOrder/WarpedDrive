959ce568d373b4ec503a2577e4f1676f2c608103
ACReeser <rogueconformist@gmail.com>
2015-09-10 20:39:58 -0500

    Added in HomeBase scene

---
+++ Assets/Harmonic/HomeBase/Scripts/HomeBase.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+using Prime31.TransitionKit;
+
+public class HomeBase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (CrossPlatformInputManager.GetButtonDown("Jump")){
+			var pixelater = new PixelateTransition()
+			{
+				nextScene = 2,
+				finalScaleEffect = PixelateTransition.PixelateFinalScaleEffect.ToPoint,
+				duration = 1.0f
+			};
+			TransitionKit.instance.transitionWithDelegate( pixelater );
+		}
+	}
+}
