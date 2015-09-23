fda451eef0b55de36d658ad2c267704ada46efe5
ACReeser <rogueconformist@gmail.com>
2015-09-23 13:37:58 -0500

    Adding in active skybox FOV zoom
    it's scaled, not 1:1

--- Assets/SniperZoom.cs
+++ Assets/SniperZoom.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 
 public class SniperZoom : MonoBehaviour {
 	public float zoomedFOV = 20f;
+	public Camera secondaryCamera;
+	public float secondaryZoomedFOV = 30f;
 
 	// Use this for initialization
 	void Start () {
 		originalFOV = Camera.main.fieldOfView;	
+		originalSecondaryFOV = secondaryCamera.fieldOfView;
 	}
 
-	private float originalFOV;
+	private float originalFOV, originalSecondaryFOV;
 
 	void Update () {
 		bool startZoom = CrossPlatformInputManager.GetButtonDown("Fire2");
@@ -19,10 +22,12 @@ public class SniperZoom : MonoBehaviour {
 		bool endZoom = CrossPlatformInputManager.GetButtonUp("Fire2");
 		if (startZoom){
 			Camera.main.fieldOfView = zoomedFOV;
+			secondaryCamera.fieldOfView = secondaryZoomedFOV;
 		}
 		if (endZoom)
 		{
 			Camera.main.fieldOfView = originalFOV;
+			secondaryCamera.fieldOfView = originalSecondaryFOV;
 		}
 	}
 }
