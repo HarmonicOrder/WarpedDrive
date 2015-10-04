73eb6c01a306f4e2814ddcc1b41b254ec8695f73
ACReeser <rogueconformist@gmail.com>
2015-10-04 14:23:16 -0500

    New server viz, skeleton subnet map, network tree script scaffolding

---
+++ Assets/Harmonic/HomeBase/Scripts/NetworkLocation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NetworkLocation {
+
+	public string Name { get; set; }
+	public string sceneName {get;set;}
+	public bool IsInfected {get;set;}
+	public NetworkLocation Parent {get;set;}
+	public IEnumerable<NetworkLocation> Children {get;set;}
+}
---
+++ Assets/Harmonic/HomeBase/Scripts/NetworkMap.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class NetworkMap {
+
+	public static Dictionary<string, NetworkLocation> RootSubnets;
+	public static IEnumerable<string> RootSubnetNames{
+		get{
+			return RootSubnets.Keys;
+		}
+	}
+
+	//static constructor!
+	static NetworkMap()
+	{
+		RootSubnets = new Dictionary<string, NetworkLocation>()
+		{
+			{
+				"Infocom", 
+				new NetworkLocation(){
+					Name = "Infocom",
+					sceneName = null
+				}
+			},
+			{
+				"Engineering", 
+				new NetworkLocation(){
+					Name = "engineering",
+					sceneName = null
+				}
+			}
+		};
+	}
+
+	private static void SetParents()
+	{
+	}
+
+}
