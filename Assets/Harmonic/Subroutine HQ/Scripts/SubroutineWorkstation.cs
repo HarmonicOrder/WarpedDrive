6da3aae911ca15999ba74e3050806bf7340e24cb
Alex Reeser <rogueconformist@gmail.com>
2015-11-23 14:21:10 -0600

    Subroutine workbench
    with scrolling code and camera angle changes

---
+++ Assets/Harmonic/Subroutine HQ/Scripts/SubroutineWorkstation.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubroutineWorkstation : MonoBehaviour {
+
+    public float rotateTime = .5f;
+
+    bool isRotating = false;
+    float currentRotateTime = 0;
+    Quaternion from, to;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isRotating)
+        {
+            this.transform.localRotation = Quaternion.Slerp(from, to, currentRotateTime / rotateTime);
+            if (currentRotateTime >= rotateTime)
+            {
+                this.transform.localRotation = to;
+                isRotating = false;
+            }
+            currentRotateTime += Time.deltaTime;
+        }
+	}
+
+    public void listingToSubroutine()
+    {
+        currentRotateTime = 0f;
+        from = this.transform.localRotation;
+        to = Quaternion.Euler(Vector3.up * 90f);
+        isRotating = true;
+    }
+
+    public void subroutineToListing()
+    {
+        currentRotateTime = 0f;
+        from = this.transform.localRotation;
+        to = Quaternion.Euler(Vector3.up);
+        isRotating = true;
+    }
+}
---
+++ Assets/Harmonic/Subroutine HQ/Scripts/YScroller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YScroller : MonoBehaviour {
+
+    public float startY = 0f;
+    public float finishY = 0f;
+    public float increment = 1f;
+    public short direction = -1;
+
+	// Use this for initialization
+	void Start () {
+        this.transform.localPosition = new Vector3(this.transform.localPosition.x, startY, this.transform.localPosition.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.localPosition.y < finishY)
+        {
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, startY, this.transform.localPosition.z);
+        }
+        this.transform.Translate(Vector3.up * increment * direction);
+	}
+}
