225b12a2a838725a3c43da7123c9723b3b5ccb76
Alex Reeser <rogueconformist@gmail.com>
2015-11-24 14:23:26 -0600

    workstation can now swap out function/movement
    it's not tied to an object right now, but it's a good proof

--- Assets/Harmonic/Subroutine HQ/Scripts/SubroutineWorkstation.cs
+++ Assets/Harmonic/Subroutine HQ/Scripts/SubroutineWorkstation.cs
@@ -1,27 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SubroutineWorkstation : MonoBehaviour {
 
     public float rotateTime = .5f;
+    public Transform subroutineVisualization;
+
+    public string currentFunctionName = "Delete";
+    public string currentMovementName = "Tracer";
+
+    private Dictionary<string, Transform> functions = new Dictionary<string, Transform>();
+    private Dictionary<string, Transform> movement = new Dictionary<string, Transform>();
 
     bool isRotating = false;
     float currentRotateTime = 0;
     Quaternion from, to;
+    Transform subParent;
+    Vector3 subFrom, subTo;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+	    foreach (Transform t in GameObject.Find("FunctionRoot").transform)
+        {
+            functions.Add(t.name, t);
+            if (t.name != currentFunctionName)
+            {
+                t.gameObject.SetActive(false);
+            }
+        }
+        foreach (Transform t in GameObject.Find("MovementRoot").transform)
+        {
+            movement.Add(t.name, t);
+            if (t.name != currentMovementName)
+            {
+                t.gameObject.SetActive(false);
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (isRotating)
         {
             this.transform.localRotation = Quaternion.Slerp(from, to, currentRotateTime / rotateTime);
+            subroutineVisualization.position = Vector3.Slerp(subFrom, subTo, currentRotateTime / rotateTime);
             if (currentRotateTime >= rotateTime)
             {
                 this.transform.localRotation = to;
+                subroutineVisualization.position = subTo;
+                subroutineVisualization.SetParent(subParent);
                 isRotating = false;
             }
             currentRotateTime += Time.deltaTime;
@@ -33,6 +61,10 @@ public class SubroutineWorkstation : MonoBehaviour {
         currentRotateTime = 0f;
         from = this.transform.localRotation;
         to = Quaternion.Euler(Vector3.up * 90f);
+        subFrom = GameObject.Find("listHarness").transform.FindChild("harness").position;
+        subParent = GameObject.Find("assemblyHarness").transform.FindChild("harness");
+        subTo = subParent.position;
+        subroutineVisualization.SetParent(null);
         isRotating = true;
     }
 
@@ -41,6 +73,34 @@ public class SubroutineWorkstation : MonoBehaviour {
         currentRotateTime = 0f;
         from = this.transform.localRotation;
         to = Quaternion.Euler(Vector3.up);
+        subFrom = GameObject.Find("assemblyHarness").transform.FindChild("harness").position;
+        subParent = GameObject.Find("listHarness").transform.FindChild("harness");
+        subTo = subParent.position;
+        subroutineVisualization.SetParent(null);
         isRotating = true;
     }
+
+    public void SetFunction(string name)
+    {
+        if (currentFunctionName != name)
+        {
+            functions[currentFunctionName].gameObject.SetActive(false);
+            functions[name].gameObject.SetActive(true);
+
+            currentFunctionName = name;
+        }
+    }
+
+    public void SetMovement(string name)
+    {
+        if (currentMovementName != name)
+        {
+            movement[currentMovementName].gameObject.SetActive(false);
+            movement[name].gameObject.SetActive(true);
+
+            currentMovementName = name;
+        }
+    }
+
+
 }
