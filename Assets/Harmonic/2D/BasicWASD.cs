fd803a7efe1d62acfc31192b4faffafd16fa4984
Alex Reeser <rogueconformist@gmail.com>
2015-12-08 18:49:09 -0600

    Fumbling with animations and tiled

--- Assets/Harmonic/2D/BasicWASD.cs
+++ Assets/Harmonic/2D/BasicWASD.cs
@@ -5,6 +5,8 @@ using Prime31.TransitionKit;
 
 public class BasicWASD : MonoBehaviour {
 
+    public Animator animator;
+
 	// Use this for initialization
 	void Start () {
 		Cursor.visible = false;
@@ -16,8 +18,24 @@ public class BasicWASD : MonoBehaviour {
 		h = CrossPlatformInputManager.GetAxis("Horizontal");
 		v = CrossPlatformInputManager.GetAxis("Vertical")  ;
 
+        if (v > 0)
+        {
+            animator.SetInteger("Direction", 2);
+        }
+        else if (v < 0)
+        {
+            animator.SetInteger("Direction", 0);
+        }
+        else if (h > 0)
+        {
+            animator.SetInteger("Direction", 3);
+        }
+        else if (h < 0)
+        {
+            animator.SetInteger("Direction", 1);
+        }
 
-		Vector3 vec = new Vector3(h, 0f, v);
+        Vector3 vec = new Vector3(h, 0f, v);
 		vec.Normalize();
 		vec.x *= Time.deltaTime * 5f;
 		vec.z *= Time.deltaTime * 5f;
