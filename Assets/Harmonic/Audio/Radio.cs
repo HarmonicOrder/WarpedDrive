23011d9f9dc82cf35aafc09b6497f81be5e7b206
Alex Reeser <rogueconformist@gmail.com>
2015-12-14 19:01:17 -0600

    adding audio and submodules

---
+++ Assets/Harmonic/Audio/Radio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Radio : MonoBehaviour {
+
+    public static Radio Instance { get; set; }
+
+	// Use this for initialization
+	void Awake () {
+        Radio.Instance = this;
+        GameObject.DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
