b8ca4ba34f017569d9947d5b44dd192b900f484c
ACReeser <rogueconformist@gmail.com>
2015-12-27 21:04:19 -0600

    StarshipEnvironment for persistent door state

---
+++ Assets/Harmonic/2D/Scripts/DoorManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorManager : MonoBehaviour {
+
+    public Transform[] Doors;
+
+    // Use this for initialization
+    void Start () {
+	    foreach(string door in StarshipEnvironment.DoorNames)
+        {
+            if (StarshipEnvironment.Instance.DoorIsOpen(door))
+            {
+                foreach(Transform t in Doors)
+                {
+                    if (t.name.ToLower().Contains(door))
+                        t.gameObject.SetActive(false);
+                }
+            }
+        }
+	}
+}
---
+++ Assets/Harmonic/Scripts/StarshipEnvironment.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StarshipEnvironment {
+
+    public Dictionary<string, bool> OpenDoors = new Dictionary<string, bool>();
+
+    private static StarshipEnvironment _instance;
+    public static StarshipEnvironment Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new StarshipEnvironment();
+            return _instance;
+        }
+    }
+
+    internal static void SetInstance(StarshipEnvironment env)
+    {
+        _instance = env;
+    }
+
+    public bool DoorIsOpen(string name)
+    {
+        return OpenDoors.ContainsKey(name) && OpenDoors[name];
+    }
+
+    public static string[] DoorNames = new string[]
+    {
+        "infosec",
+        "cargobay",
+        "biomed",
+        "bridge"
+    };
+}
