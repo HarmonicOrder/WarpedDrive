b37dbe64a4c474278eb473847e9f64940c802266
Alex Reeser <rogueconformist@gmail.com>
2016-01-20 14:25:30 -0600

    RI Animations barely work
    But transitions are all messed up. Thanks for the poor documentation,
    Mecanim team! Not up to par with the rest of Unity :(

--- Assets/Harmonic/Gui/RI/AIRenderer.cs
+++ Assets/Harmonic/Gui/RI/AIRenderer.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
 
 //idea: inherit this for a VirusAIRenderer, something that talks back
 //how to sync between them? I have no idea
@@ -9,6 +11,7 @@ using System.Collections.Generic;
 // AIRenderer.Instance.Output("you stink")
 // return new waitforseconds
 // VirusAIRenderer.Instance.Output("no you stink")
+[RequireComponent(typeof(Image))]
 public class AIRenderer : MonoBehaviour {
     public Sprite TalkSprite, ThinkSprite, IdeaSprite, SearchingSprite, AlertSprite, TimerAlertSprite;
     public Color NormalColor;
@@ -21,32 +24,144 @@ public class AIRenderer : MonoBehaviour {
 
     private Queue<Tuple<RIState, string>> Queue = new Queue<Tuple<RIState, string>>();
     private Queue<Tuple<RIState, string>> PriorityQueue = new Queue<Tuple<RIState, string>>();
-
+    private Image behaviorImage;
     // Use this for initialization
     void Awake () {
         Instance = this;
+        behaviorImage = GetComponent<Image>();
 	}
 
     //todo: maybe make coroutines instead
+    void Start()
+    {
+        StartCoroutine(PollQueues());
+    }
+
+    private string[] sentences;
+    private int sentenceIndex;
+    private Regex splitter = new Regex(@"(\.|\?|\!)");
+    private IEnumerator PollQueues()
+    {
+        while(this.isActiveAndEnabled)
+        {
+            Tuple<RIState, string> CurrentOutput = null;
+
+            //pull priority queue
+            if (PriorityQueue.Count > 0)
+                CurrentOutput = PriorityQueue.Dequeue();
+            else if (Queue.Count > 0)
+                CurrentOutput = Queue.Dequeue();
+
+            //if not in idle and has priority
+            //apologize (this just in...uh sorry but....oh....new data incoming....)
+            //show priority
+            //reentry (anyway...so anyways...as I was saying...)
+            //else
+            if (CurrentOutput != null)
+            {
+                //if finished with last state
+
+                //set sprite to state
+                behaviorImage.sprite = GetSprite(CurrentOutput.First);
+                //set animator to corresponding animation state
+                AnimationAnimator.SetInteger("CurrentState", (int)CurrentOutput.First);
+                AnimationAnimator.SetFloat("CurrentSpeed", GetSpeed(CurrentOutput.First));
+
+                OutputText.text = "";
+                //CurrentOutput.Second.Split('.', '?', '!');
+                sentences = splitter.Split(CurrentOutput.Second);
+                //set text (one sentence at a time?)
+                for (sentenceIndex = 0; sentenceIndex < sentences.Length; sentenceIndex++)
+                {
+                    OutputText.text += sentences[sentenceIndex];
+
+                    //wait based on how many characters are in the output string
+                    yield return new WaitForSeconds(sentences[sentenceIndex].Length * .1f);
+                }
+            }
+            else
+            {
+                yield return new WaitForSeconds(.5f);
+            }
+            //repeat
+
+            //maybe include debug input (1-6) to set states
+
+        }
+    }
+
+    private float GetSpeed(RIState first)
+    {
+        switch(first)
+        {
+            case RIState.Alerting:
+                return 1;
+            case RIState.Idea:
+                return 1;
+            case RIState.Searching:
+                return -2;
+            case RIState.Talking:
+                return 1;
+            case RIState.Thinking:
+                return -2;
+            case RIState.Timing:
+                return 1;
+            default:
+                return 1;
+        }
+    }
+
+#if UNITY_EDITOR
     void Update()
     {
-        //pull priority queue
-        //if not in idle and has priority
-        //apologize (this just in...uh sorry but....oh....new data incoming....)
-        //show priority
-        //reentry (anyway...so anyways...as I was saying...)
-        //else
-        //if finished with last state
-        //set sprite to state
-        //set animator to corresponding animation state
-        //set text (one sentence at a time?)
-        //wait
-        //  might even could wait based on how many characters are in the output string
-        //repeat
-
-        //maybe include debug input (1-6) to set states
+        if (Input.GetKeyUp(KeyCode.Alpha0))
+        {
+            PriorityQueue.Enqueue(new Tuple<RIState, string>(RIState.Alerting, "There's a snake in my boot!"));
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            Queue.Enqueue(new Tuple<RIState, string>(RIState.Idea, "Eureka! I think I've cracked it, good sport."));
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            Queue.Enqueue(new Tuple<RIState, string>(RIState.Searching, "Looking around...can't say I see much. Oh! Nope, I was wrong."));
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            Queue.Enqueue(new Tuple<RIState, string>(RIState.Talking, "Here we can see the programmer in his native habitat. Filled with Mountain Dew. And video games."));
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            Queue.Enqueue(new Tuple<RIState, string>(RIState.Thinking, "Hmmmm. Hmmmmmmmm. Hhhhmmmmm..."));
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha5))
+        {
+            Queue.Enqueue(new Tuple<RIState, string>(RIState.Timing, "My oh my look at the time! You're running low on oxygen..."));
+        }
     }
-	
+#endif
+
+    private Sprite GetSprite(RIState first)
+    {
+        switch(first)
+        {
+            case RIState.Alerting:
+                return AlertSprite;
+            case RIState.Idea:
+                return IdeaSprite;
+            case RIState.Searching:
+                return SearchingSprite;
+            case RIState.Talking:
+                return TalkSprite;
+            case RIState.Thinking:
+                return ThinkSprite;
+            case RIState.Timing:
+                return TimerAlertSprite;
+            default:
+                return null;
+        }
+    }
+
     //for conversations it might make sense to return how long the duration for saying the text is?
     //could just be a lookup
     public void Output(RIState state, string text, bool isPriority = false)
@@ -61,5 +176,5 @@ public class AIRenderer : MonoBehaviour {
         }
     }    
 
-    public enum RIState { Idle, Talking, Thinking, Searching, Idea, Timing, Alerting }
+    public enum RIState { Idle = 0, Talking, Thinking, Searching, Idea, Timing, Alerting }
 }
