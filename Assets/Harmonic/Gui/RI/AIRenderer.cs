9f11c44cbc0af395a49af5ff6ac47502f08dec83
ACReeser <rogueconformist@gmail.com>
2016-01-19 23:25:08 -0600

    More icons, more animations, all for RI tutorial

---
+++ Assets/Harmonic/Gui/RI/AIRenderer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AIRenderer : MonoBehaviour {
+    public Sprite TalkSprite, ThinkSprite, IdeaSprite, SearchingSprite, AlertSprite, TimerAlertSprite;
+    public Color NormalColor;
+    public Color AlertColor;
+    public Text OutputText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
