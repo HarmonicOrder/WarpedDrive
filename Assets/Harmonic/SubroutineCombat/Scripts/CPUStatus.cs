59ae549039fb058cede416dea24347b5d8031618
Alex Reeser <rogueconformist@gmail.com>
2016-02-11 18:51:12 -0600

    Working on new, intuitive CPU bar

--- Assets/Harmonic/SubroutineCombat/Scripts/CPUStatus.cs
+++ Assets/Harmonic/SubroutineCombat/Scripts/CPUStatus.cs
@@ -2,28 +2,45 @@
 using System.Collections;
 using UnityEngine.UI;
 using System;
+using System.Collections.Generic;
 
 public class CPUStatus : MonoBehaviour {
 	public float MaxWidth = 500f;
 	public Text CPUTextLine;
-	public RectTransform CPUBar;
+	public RectTransform CPUBar, InUse, NotInUse, Occupied, Infected;
     public Image CPUUsedImage;
     public Image CPUUsedByVirusImage;
+
+    private List<RectTransform> InUses, NotInUses, Occupieds, Infecteds;
 
 	// Use this for initialization
 	void Start () {
 		CyberspaceBattlefield.Current.OnCoreChange += OnCPUChange;
 		OnCPUChange();
-	}
+        InUses = new List<RectTransform>();
+        NotInUses = new List<RectTransform>();
+        Occupieds = new List<RectTransform>();
+        Infecteds = new List<RectTransform>();
+    }
 
 	public void OnCPUChange()
 	{
         string newCPUString = string.Format("{0} / {1} / {2} CPU ¢ores", CyberspaceBattlefield.Current.UsedCores, CyberspaceBattlefield.Current.CurrentCores, CyberspaceBattlefield.Current.TotalCores);
-		//float denom = Mathf.Max(CyberspaceBattlefield.Current.CurrentCores, 1f);
-		//SetBarByPercentage(CyberspaceBattlefield.Current.UsedCores / denom);
-        SetCPUBarImageFilled();
 		CPUTextLine.text = newCPUString;
+        //float denom = Mathf.Max(CyberspaceBattlefield.Current.CurrentCores, 1f);
+        //SetBarByPercentage(CyberspaceBattlefield.Current.UsedCores / denom);
+        //SetCPUBarImageFilled();
+        RefreshCPURectangles();
 	}
+
+    private void RefreshCPURectangles()
+    {
+        print("Refreshing cpus");
+        foreach(RectTransform t in this.transform)
+        {
+            
+        }
+    }
 
     private void SetCPUBarImageFilled()
     {
