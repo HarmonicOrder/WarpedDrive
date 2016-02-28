a4a23643e3d85883fef2d3ef941cd85c466088de
ACReeser <rogueconformist@gmail.com>
2016-02-28 17:47:19 -0600

    Fix for bombs not replaying particles

--- Assets/Harmonic/SubroutineCombat/Scripts/LagBomb.cs
+++ Assets/Harmonic/SubroutineCombat/Scripts/LagBomb.cs
@@ -7,6 +7,7 @@ public class LagBomb : MonoBehaviour {
 
     public GameObject ExplosionRoot;
     public GameObject ShellRoot;
+    public ParticleSystem ps1, ps2, ps3;
     private bool firing;
     private HarmonicUtils.LerpContext BombLerp;
 
@@ -48,7 +49,16 @@ public class LagBomb : MonoBehaviour {
         LagTargetsInProximity(this.transform.position);
 
         ExplosionRoot.SetActive(true);
-        ExplosionRoot.GetComponent<ParticleSystem>().Play(true);
+        //doesn''t work!
+        //ExplosionRoot.GetComponent<ParticleSystem>().Play(true);
+        ps1.Play();
+        //it's a bug,
+        //https://issuetracker.unity3d.com/issues/particle-system-plays-only-once
+        //how the heck is this a workaround
+        ps2.Play();
+        ps2.startLifetime = ps2.startLifetime;
+        ps3.Play();
+        ps3.startLifetime = ps3.startLifetime;
 
         ShellRoot.SetActive(false);
         StartCoroutine(LetExplode());
@@ -57,7 +67,12 @@ public class LagBomb : MonoBehaviour {
     private IEnumerator LetExplode()
     {
         yield return new WaitForSeconds(3f);
-        ExplosionRoot.GetComponent<ParticleSystem>().Stop(true);
+        ps1.Stop();
+        ps2.Stop();
+        ps3.Stop();
+        //doesn't work!
+        //ExplosionRoot.GetComponent<ParticleSystem>().Stop(true);
+        //ExplosionRoot.GetComponent<ParticleSystem>().Clear(true);
     }
 
     private void LagTargetsInProximity(Vector3 position)
