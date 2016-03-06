3c359d328123f7873c56e120124357fbef7cda30
ACReeser <rogueconformist@gmail.com>
2016-03-06 14:31:08 -0600

    New WIFI visualization

---
+++ Assets/Harmonic/SubroutineCombat/Scripts/AnimationDelay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AnimationDelay : MonoBehaviour {
+
+    public float Delay = 1f;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Animation>().playAutomatically = false;
+        StartCoroutine(WaitThenPlay());
+	}
+
+    private IEnumerator WaitThenPlay()
+    {
+        yield return new WaitForSeconds(Delay);
+        this.GetComponent<Animation>().Play();
+        this.enabled = false;
+    }
+}
