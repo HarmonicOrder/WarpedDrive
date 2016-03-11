2d6eb5a729a6310fd1fd22639ff786ed8a76d51d
ACReeser <rogueconformist@gmail.com>
2016-03-10 21:51:16 -0600

    New wifi level

--- Assets/Harmonic/HomeBase/Scripts/NetworkMap.cs
+++ Assets/Harmonic/HomeBase/Scripts/NetworkMap.cs
@@ -249,6 +249,38 @@ public static class NetworkMap {
                                     IsInfected = true
                                 }
                             }
+                        },
+                        new NetworkLocation(){
+                            Name = "wifi",
+                            sceneIndex = 15,
+                            IsInfected = true,
+                            Machines = new List<Machine>()
+                            {
+                                new Machine()
+                                {
+                                    Name = "GatewayMachine",
+                                    CPUCores = 2,
+                                    IsInfected = false
+                                },
+                                new Machine()
+                                {
+                                    Name = "SecurityMachine",
+                                    CPUCores = 4,
+                                    IsInfected = true
+                                },
+                                new Machine()
+                                {
+                                    Name = "ArchiveMachine",
+                                    CPUCores = 2,
+                                    IsInfected = true
+                                },
+                                new Machine()
+                                {
+                                    Name = "ServerMachine",
+                                    CPUCores = 2,
+                                    IsInfected = true
+                                }
+                            }
                         }
                     }
                 }
